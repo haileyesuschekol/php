65edfccd7fe393bb2991ffd6ba9d1111e01261a8
Haileyesus Chekol <teamdevs7@gmail.com>
2024-10-07 18:02:46 +0100

    functions

--- index.php
+++ index.php
@@ -14,8 +14,7 @@ echo "Hello $greeting";
 
 $book = true;
 
-
-
+//conditional
 if($book){
     $message =  " you have read it";
 }else{
@@ -24,5 +23,38 @@ if($book){
 ?>
 
 <h2> <?php echo $message?> </h2>
+
+<?php
+$contacts = [
+    ["name"=>"John",
+     "age"=>18,
+     "website"=>"https://example.com"
+    ],
+    ["name"=>"Doe",
+     "age"=>28,
+     "website"=>"https://example.com"
+    ],
+];
+
+ function filterByName($contacts, $name){
+   $searchName = [];
+
+   foreach($contacts as $contact){
+    if($contact["name"] === $name){
+        $searchName[] = $contact;
+    }
+}
+return $searchName;
+
+}
+?>
+
+<ul>
+    <?php foreach(filterByName( $contacts, "John") as $contact):?>
+     <li>  <?= $contact["name"]?></li>
+     <li>  <?= $contact["age"]?></li>
+    <a href="<?= $contact["website"]?>">contact</a>
+    <?php endforeach; ?>
+</ul>
 </body>
 </html>

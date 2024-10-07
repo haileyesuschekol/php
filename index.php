2a4017ef62616288c01a82dc7c6e3182f47fad71
Haileyesus Chekol <teamdevs7@gmail.com>
2024-10-07 15:28:17 +0100

    conditional

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    
+    
+    <?php
 $greeting = "php";
 echo "Hello $greeting";
+
+$book = true;
+
+
+
+if($book){
+    $message =  " you have read it";
+}else{
+    $message = "you have to read it!";
+}
 ?>
+
+<h2> <?php echo $message?> </h2>
+</body>
+</html>

586a5e73f0a846983782f716058c1cabd712ff79
Haileyesus Chekol <teamdevs7@gmail.com>
2024-10-08 20:08:06 +0100

    refactor

--- index.php
+++ index.php
@@ -1,45 +1,4 @@
     <?php
-$greeting = "php";
-// echo "Hello $greeting";
-
-$book = true;
-
-//conditional
-if($book){
-    $message =  " you have read it";
-}else{
-    $message = "you have to read it!";
-}
-
-//array
-$contacts = [
-    ["name"=>"John",
-     "age"=>18,
-     "website"=>"https://example.com"
-    ],
-    ["name"=>"Doe",
-     "age"=>28,
-     "website"=>"https://example.com"
-    ],
-];
-
-//  function filterByName($contacts, $key, $value){
-//    $searchName = [];
-
-//    foreach($contacts as $contact){
-//     if($contact[$key] === $value){
-//         $searchName[] = $contact;
-//     }
-// }
-// return $searchName;
-
-// }
-
-
-// array_filter function
-$filteredData = array_filter($contacts, function($data){
-   return $data['name'] === "John";
-});
 
 require("url.php");
 $heading = "Home";
--- views/contact.view.php
+++ views/contact.view.php
@@ -11,23 +11,3 @@
    
     <?php require("partials/footer.php")?>
 
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
-</head>
-<body>
-    
-    
-
-<ul>
-    <?php foreach($filteredData as $contact):?>
-     <li>  <?= $contact["name"]?></li>
-     <li>  <?= $contact["age"]?></li>
-    <a href="<?= $contact["website"]?>">contact</a>
-    <?php endforeach; ?>
-</ul>
-</body>
-</html> -->
